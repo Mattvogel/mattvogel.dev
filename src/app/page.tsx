b267e5b96b158cf274eff2b592e9a990cf353e80
Matt Vogel <matthew.scott.vogel@gmail.com>
2023-06-21 16:29:40 +1000

    fix: lightmode and darkmode text modification also modified background CSS to scale with pages past 100% this stops the weird box appearing between the background image and the footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,12 +14,10 @@ export default function Home() {
     <>
     <main className="static min-h-screen flex-col items-center justify-between">
       <div className='absolute bottom-20 left-20'>
-        <h1 className={`mb-4 text-4xl font-extrabold leading-non tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white select-none font-rock ${rock.variable}`}>Matthew Vogel</h1>
+        <h1 className={`mb-4 text-4xl font-extrabold leading-non tracking-tight text-white md:text-5xl lg:text-6xl dark:text-white select-none font-rock ${rock.variable}`}>Matthew Vogel</h1>
       </div>
     </main>
     <StaticFooter/>
     </>
   )
 }
-
-//<span className="random-word mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400"></span>
